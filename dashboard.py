b90dbc63acd59adbe4227c61239df77f0d05009f
LabibAF <labibfadhali12@gmail.com>
2023-10-02 12:21:32 +0700

    fix code

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 import numpy as np
+import matplotlib.pyplot as plt
 from babel.numbers import format_currency
 sns.set_style('dark')
 
